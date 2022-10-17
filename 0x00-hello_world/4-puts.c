c7602bbf2b10bc2d282a8c96fcb3ea21c57b9109
Dnju <daviddnju@gmail.com>
2022-10-17 00:58:11 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
@@ -7,3 +16,5 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
+
+}
