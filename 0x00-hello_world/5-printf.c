708a73b5a2f97093a7bc97af4bad9fe03287f409
Dnju <daviddnju@gmail.com>
2022-10-17 02:04:22 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
+ *main - Prints out a string to stdout.
  *
- * main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
+ *Return: 0 if success.
  *
  */
 int main(void)
