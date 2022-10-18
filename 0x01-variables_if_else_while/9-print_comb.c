6f15a135709dcb5f04aaaeba10b683ca200e5398
Dnju <daviddnju@gmail.com>
2022-10-17 23:19:25 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -18,14 +18,19 @@ for (d = '0'; d <= '9'; d++)
 
 {
 putchar(d);
+
+
 if (d != '9')
+
 {
-										putchar(',');							
+										putchar(',')
 										putchar(' ');
 										}
 										}
 
+
 putchar('\n');
+
 
 return (0);
 
