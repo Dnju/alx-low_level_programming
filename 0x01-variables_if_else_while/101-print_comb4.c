06c4d0036c10dc1a5daa57ca0547d3d9d46fb591
Dnju <daviddnju@gmail.com>
2022-10-18 02:50:28 -0700

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,13 +13,18 @@ while (i <= '7')
 {
 while (j <= '8')
 {
-while (k <= '9')								{
+
+while (k <= '9')
+
+{
 if (i < j && j < k)
 {
+
 putchar(i);
 putchar(j);						 
 putchar(k);
 if (!(i == '7' && j == '8' && k == '9'))					{
+
 putchar(',');
 putchar(' ');
 }									
