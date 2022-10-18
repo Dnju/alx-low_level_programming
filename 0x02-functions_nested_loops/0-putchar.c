8d4ee61c80f62fc07127df534d88dab712475457
Dnju <daviddnju@gmail.com>
2022-10-18 05:41:25 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * main - Entry point
+ *main - Entry point
  *
  *Return: Always 0 (Success)
  *
  */
 int main(void)
 {
-char *sh = "Holberton";
+char *sh = "putchar";
 while (*sh)
 {
 _putchar(*sh);
