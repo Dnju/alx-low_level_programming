895343f3832a5f77b36c720577b8d91e05e3b95f
Dnju <daviddnju@gmail.com>
2022-10-17 23:52:55 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,23 @@
  */
 int main(void)
 {
-	int d;
+int d;
 
-	for (d = '0'; d <= '9'; d++)
-		putchar(d);
-	        if (d != '9')
-		{
-		            putchar(',');
-		            putchar(' ');
-		}	
+for (d = '0'; d <= '9'; d++)
+{
+
+putchar(d);
+
+if (d != '9')
+{
+
+putchar(',');
+putchar(' ');
+
 }
+
+}
+
 putchar('\n');
 
 return (0);
