511af396e87b971fc083147c8d528ed2c2dd9c35
Dnju <daviddnju@gmail.com>
2022-10-18 23:40:27 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main -  prints the english alphabet from a-z.
+ *
+ * return: Nothing
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+   putchar(c); 
+}
+_putchar('\n');
+}	
