21d406a69656cf61bad520f0b7c61ab934e26235
Dnju <daviddnju@gmail.com>
2022-10-19 02:20:46 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,10 +14,9 @@ void print_alphabet_x10(void)
 
 	{
 
-        	for (x = 'a'; x <= 'z'; x++)
+        		for (x = 'a'; x <= 'z'; x++)
+	        		_putchar(x);
 
-	        	_putchar(x);
-
-		_putchar('\n');
+			_putchar('\n');
 	}
 }
