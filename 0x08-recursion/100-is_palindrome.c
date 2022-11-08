d19de3e08d5cdfac244de0443f28d3e9262ad3a5
Dnju <daviddnju@gmail.com>
2022-11-08 06:17:10 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,17 +1,32 @@
-#include "main.h"
+include "main.h"
 /**
-*is_palindrome - find a palindrome string
-*@s: string for check
+*recurse - palindrome check
+*@start: character to check with end
+*@end: character to check with start
 *
-*Return: 1 if is palindrome, otherwise 0
+*Return: Palindrome 1, Not 0
+*/
+int recurse(char *start, char *end){if (start == end)return (1);if (*start == *end && start + 1 == end)return (1);if (*start == *end)return (recurse(start + 1, end - 1));return (0);}
+/**
+*end - return a pointer at the end of a string
+*@s: string reach end of
+*
+*Return: pointer to end of string
+*/
+char *end(char *s)
+{
+	if (*s)
+		return (end(s + 1));
+	return (s);
+}
+/**
+*is_palindrome - check if string is a palindrome
+*@s: string to check
+*
+* Return: True 1, False 0
 */
 int is_palindrome(char *s)
 {
-	int i;
+	return (recurse(s, end(s) - 1));
 
-	i = 0;
-
-	if (*s == i)
-		return (0);
-	return (1);
 }
