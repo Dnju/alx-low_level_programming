4fda9578bc2d39d8fef34c35708dafd2bc2896bd
Dnju <daviddnju@gmail.com>
2022-11-09 14:38:30 -0800

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -39,13 +39,12 @@ int **alloc_grid(int width, int height)
 		}
 	}
 
-		for (k = 0; k < height; k++;)
+	for (k = 0; k < height; k++)
+	{
+		for (l = 0; l < width; l++)
 		{
-			for (l = 0; l < width; l++)
-			{
-				a[k][l] = 0;
-			}
+			a[k][l] = 0;
 		}
-
+	}
 	return (a);
 }
