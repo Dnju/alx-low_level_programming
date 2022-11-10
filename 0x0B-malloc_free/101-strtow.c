bbe60ec6d32df9c40cdfc8828c613c3ff9693445
Dnju <daviddnju@gmail.com>
2022-11-09 19:10:10 -0800

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
 *_isspace - check if a character is whitespace
 *@c: the character to check
- *
-*Return: 1 is c is a whitespace character, otherwise 0
 *
+*Return: 1 is c is a whitespace character, otherwise 0
 */
+
 int _isspace(int c)
 {
 	if (c == 0x20 || (c >= 0x09 && c <= 0x0d))
